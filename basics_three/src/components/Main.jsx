cf88a49fede86849f82847c67e8a30a8f4d66e42
haroon1405 <mohammedharoonmh2000@gmail.com>
2024-01-17 17:43:44 +0300

    urls added

--- basics_three/src/components/Main.jsx
+++ basics_three/src/components/Main.jsx
@@ -1,21 +1,37 @@
 import React from "react";
+import imgData from '../imgsdata.js' 
 
 export default function Main(){
+
+    let imgList = "";
+        let randomIndex = 0;
+        let imgURL = "";
+
+    function handleClick(){
+        imgList = imgData.imagesURL;
+        randomIndex = Math.floor(Math.random() * imgList.length)
+        imgURL = imgList[randomIndex].url
+    }
+
     return(
-        <div>
-            <div className="w-fit mx-auto mt-[100px] space-y-10">
-                <div className="space-x-[200px]">
+        <div className="w-1/2 mx-auto">
+            <div className="w-full mx-auto mt-[50px] space-y-10">
+                <div className="flex items-center justify-between">
                     <label className="text-2xl" htmlFor="fname">First Name</label>
-                    <input id="fname" type="text" name="fname" className="border-2 p-2 rounded-md w-[300px]"></input>
+                    <input id="fname" type="text" name="fname" className="border-2 p-2 rounded-md w-[300px] text-gray-700 text-xl"></input>
                 </div>
-                <div className="space-x-[200px]">
+                <div className="flex items-center justify-between">
                     <label className="text-2xl" htmlFor="lname">Last Name</label>
-                    <input id="lname" type="text" name="lname" className="border-2 p-2 rounded-md w-[300px]"></input>
+                    <input id="lname" type="text" name="lname" className="border-2 p-2 rounded-md w-[300px] text-gray-700 text-xl"></input>
                 </div>
-            </div>
-            <div className="w-fit mx-auto mt-[55px] px-7 py-2 rounded-lg text-2xl bg-blue-500 hover:bg-blue-400 active:bg-blue-700 text-white">
-                    Click
+                <button onClick={handleClick} className="block w-full px-7 py-2 rounded-lg text-2xl bg-blue-500 hover:bg-blue-400 active:bg-blue-700 text-white">
+                    Display 
+                </button>
+                <img className="w-full rounded-lg" src="https://images.unsplash.com/photo-1501436513145-30f24e19fcc8?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=900&ixid=MnwxfDB8MXxyYW5kb218MHx8bmF0dXJlfHx8fHx8MTcwNTUwMDgzMw&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1600" alt="nature" />
             </div>
         </div>
     )
 }
+
+
+// https://source.unsplash.com/1600x900/?nature
---
+++ basics_three/src/imgsdata.js
@@ -0,0 +1,44 @@
+export default {
+    "imagesURL": [
+        {
+            "id" : 1,
+            "url": "https://images.unsplash.com/photo-1504198266287-1659872e6590?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=900&ixid=MnwxfDB8MXxyYW5kb218MHx8bmF0dXJlfHx8fHx8MTcwNTUwMTE2NA&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1600"
+        },
+        {
+            "id" : 2,
+            "url": "https://images.unsplash.com/reserve/HgZuGu3gSD6db21T3lxm_San%20Zenone.jpg?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=900&ixid=MnwxfDB8MXxyYW5kb218MHx8bmF0dXJlfHx8fHx8MTcwNTUwMTE2Mg&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1600"
+        },
+        {
+            "id" : 3,
+            "url": "https://images.unsplash.com/photo-1438786657495-640937046d18?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=900&ixid=MnwxfDB8MXxyYW5kb218MHx8bmF0dXJlfHx8fHx8MTcwNTUwMTE2MQ&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1600"
+        },
+        {
+            "id" : 4,
+            "url": "https://images.unsplash.com/photo-1533119408463-b0f487583ff6?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=900&ixid=MnwxfDB8MXxyYW5kb218MHx8bmF0dXJlfHx8fHx8MTcwNTUwMTE1Mg&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1600"
+        },
+        {
+            "id" : 5,
+            "url": "https://images.unsplash.com/photo-1490750967868-88aa4486c946?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=900&ixid=MnwxfDB8MXxyYW5kb218MHx8bmF0dXJlfHx8fHx8MTcwNTUwMTE0Nw&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1600"
+        },
+        {
+            "id" : 6,
+            "url": "https://images.unsplash.com/photo-1470071459604-3b5ec3a7fe05?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=900&ixid=MnwxfDB8MXxyYW5kb218MHx8bmF0dXJlfHx8fHx8MTcwNTUwMTE0NQ&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1600"
+        },
+        {
+            "id" : 7,
+            "url": "https://images.unsplash.com/photo-1507608616759-54f48f0af0ee?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=900&ixid=MnwxfDB8MXxyYW5kb218MHx8bmF0dXJlfHx8fHx8MTcwNTUwMTE0Mw&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1600"
+        },
+        {
+            "id" : 8,
+            "url": "https://images.unsplash.com/photo-1447752875215-b2761acb3c5d?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=900&ixid=MnwxfDB8MXxyYW5kb218MHx8bmF0dXJlfHx8fHx8MTcwNTUwMTE0MQ&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1600"
+        },
+        {
+            "id" : 9,
+            "url": "https://images.unsplash.com/photo-1542856391-010fb87dcfed?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=900&ixid=MnwxfDB8MXxyYW5kb218MHx8bmF0dXJlfHx8fHx8MTcwNTUwMTE0MA&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1600"
+        },
+        {
+            "id" : 10,
+            "url": "https://images.unsplash.com/photo-1500829243541-74b677fecc30?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=900&ixid=MnwxfDB8MXxyYW5kb218MHx8bmF0dXJlfHx8fHx8MTcwNTUwMTEzNw&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1600"
+        },
+    ]
+}
