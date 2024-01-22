a8963b37d7d14f23d30c807e8132b01af20194e9
haroon1405 <mohammedharoonmh2000@gmail.com>
2024-01-22 22:14:52 +0300

    minor

--- newsletter/src/index.js
+++ newsletter/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <h1 className='text-center text-4xl'>Newsletter App</h1>
+  <>
+  <h1 className='text-center text-4xl mt-3'>NewsLetter App</h1>
+  </>
 );
 
